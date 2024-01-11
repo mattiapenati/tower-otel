93e3a68177c28a3969576a887053403d8401d1f2
Mattia Penati <mattia.penati@protonmail.com>
2024-01-11 23:04:25 +0100

    HTTP example updated to use `hyper` 1

--- examples/http/src/main.rs
+++ examples/http/src/main.rs
@@ -1,6 +1,13 @@
+use std::{
+    future::{Future, IntoFuture},
+    pin::Pin,
+    task::{ready, Context, Poll},
+};
+
 use axum::{http::Request, routing::get, Router};
-use hyper::Client;
+use http_body_util::{BodyExt, Empty};
 use opentelemetry_sdk::{propagation::TraceContextPropagator, runtime::Tokio};
+use pin_project::pin_project;
 use tower::{Service, ServiceBuilder, ServiceExt};
 use tower_otel::trace::HttpLayer;
 use tracing::Level;
@@ -50,20 +57,107 @@ async fn main() {
     let app = Router::new()
         .route("/", get(|| async { "Hello, World!" }))
         .layer(HttpLayer::server(Level::DEBUG));
-    let server = axum::Server::bind(&"[::1]:3000".parse().unwrap()).serve(app.into_make_service());
+    let listener = tokio::net::TcpListener::bind("[::1]:3000").await.unwrap();
+    let server = axum::serve(listener, app).into_future();
     tokio::spawn(server);
 
+    let tcp = tokio::net::TcpStream::connect("[::1]:3000").await.unwrap();
+    let io = TokioIo(tcp);
+    let (request_sender, connection) = hyper::client::conn::http1::handshake(io).await.unwrap();
+    tokio::spawn(connection);
     let mut client = ServiceBuilder::new()
         .layer(HttpLayer::client(Level::DEBUG))
-        .service(Client::new());
+        .service(Client(request_sender));
 
     let req = Request::get("http://[::1]:3000")
-        .body(Default::default())
+        .body(Empty::<&[u8]>::new())
         .unwrap();
     let res = client.ready().await.unwrap().call(req).await.unwrap();
-    let body = hyper::body::to_bytes(res.into_body()).await.unwrap();
+    let body = res.collect().await.unwrap().to_bytes();
     let body = std::str::from_utf8(&body).unwrap();
     tracing::info!("received '{}'", body);
 
     opentelemetry::global::shutdown_tracer_provider();
+}
+
+struct Client<B>(hyper::client::conn::http1::SendRequest<B>);
+
+impl<B> tower::Service<hyper::Request<B>> for Client<B>
+where
+    B: hyper::body::Body + 'static,
+{
+    type Response = hyper::Response<hyper::body::Incoming>;
+    type Error = hyper::Error;
+    type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>>>>;
+
+    fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        self.0.poll_ready(cx)
+    }
+
+    fn call(&mut self, req: hyper::Request<B>) -> Self::Future {
+        Box::pin(self.0.send_request(req))
+    }
+}
+
+#[pin_project]
+struct TokioIo<T>(#[pin] T);
+
+impl<T> hyper::rt::Read for TokioIo<T>
+where
+    T: tokio::io::AsyncRead,
+{
+    fn poll_read(
+        self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        mut buf: hyper::rt::ReadBufCursor<'_>,
+    ) -> Poll<Result<(), std::io::Error>> {
+        let n = {
+            let this = self.project();
+            let mut buf = tokio::io::ReadBuf::uninit(unsafe { buf.as_mut() });
+            ready!(this.0.poll_read(cx, &mut buf))?;
+            buf.filled().len()
+        };
+        unsafe { buf.advance(n) };
+        Poll::Ready(Ok(()))
+    }
+}
+
+impl<T> hyper::rt::Write for TokioIo<T>
+where
+    T: tokio::io::AsyncWrite,
+{
+    fn poll_write(
+        self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        buf: &[u8],
+    ) -> Poll<Result<usize, std::io::Error>> {
+        let this = self.project();
+        this.0.poll_write(cx, buf)
+    }
+
+    fn poll_flush(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), std::io::Error>> {
+        let this = self.project();
+        this.0.poll_flush(cx)
+    }
+
+    fn poll_shutdown(
+        self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+    ) -> Poll<Result<(), std::io::Error>> {
+        let this = self.project();
+        this.0.poll_shutdown(cx)
+    }
+
+    fn is_write_vectored(&self) -> bool {
+        self.0.is_write_vectored()
+    }
+
+    fn poll_write_vectored(
+        self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        bufs: &[std::io::IoSlice<'_>],
+    ) -> Poll<Result<usize, std::io::Error>> {
+        let this = self.project();
+        this.0.poll_write_vectored(cx, bufs)
+    }
 }
