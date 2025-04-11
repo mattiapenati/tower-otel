310ba4a78cfc847a6a7269651e966dd828a174ce
Mattia Penati <mattia.penati@protonmail.com>
2025-04-11 18:37:02 +0200

    Add reference to OpenTelemetry conventions

--- src/trace/mod.rs
+++ src/trace/mod.rs
@@ -1,6 +1,11 @@
 //! Middleware that adds tracing to a [`Service`].
 //!
+//! These middlewares follow the conventions defined by OpenTelemetry for [HTTP protocol] and [gRPC
+//! protocol].
+//!
 //! [`Service`]: tower_service::Service
+//! [HTTP protocol]: https://opentelemetry.io/docs/specs/semconv/http/http-spans/
+//! [gRPC protocol]: https://opentelemetry.io/docs/specs/semconv/rpc/grpc/
 
 #[doc(inline)]
 pub use self::{
