310bf429f0760627e98026a5a11205ef3627f910
Mattia Penati <mattia.penati@protonmail.com>
2025-04-12 22:37:54 +0200

    Add crate's features documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,16 @@
+/*!
+
+# Crate features
+
+* **axum** -
+  Enables the [`axum`] integration. Trace and metrics will contain the
+  `http.route` attribute, populated with the path in the router that matches
+  the request.
+
+[`axum`]: https://docs.rs/axum
+
+*/
+
 pub mod metrics;
 pub mod trace;
 mod util;
