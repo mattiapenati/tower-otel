2ec3206273b29644e1ded32e60371f5b3c64106b
Mattia Penati <mattia.penati@protonmail.com>
2025-10-01 22:19:47 +0200

    Update documentation about axum feature

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@
 * **axum** -
   Enables the [`axum`] integration. Trace and metrics will contain the
   `http.route` attribute, populated with the path in the router that matches
-  the request.
+  the request, as well as the `client.address` and `client.port` attributes.
 
 [`axum`]: https://docs.rs/axum
 
